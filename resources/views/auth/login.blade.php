2a96faff3673b9fe9b687dbc7ae6ace71b00632a
Fandi Nurrokhim <fandinurrokhim56@Gmail.com>
2025-05-21 21:21:07 +0700

    change to id

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,10 +23,10 @@
             <div class="mb-3">
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" id="remember-me" />
-                    <label class="form-check-label" for="remember-me"> Remember Me </label>
+                    <label class="form-check-label" for="remember-me">Ingat Saya </label>
                 </div>
             </div>
-            <button class="btn btn-primary d-grid w-100">Sign in</button>
+            <button class="btn btn-primary d-grid w-100">Login</button>
         </form>
     </div>
 @endsection
